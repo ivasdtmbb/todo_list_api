cef1f10d5d5ee5b7471f8948d1bc99a6303450d2
admin <ivasdtmbb@gmail.com>
2025-02-11 01:02:11 +1000

    Base structure of the Project

---
+++ cmd/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	
+)
+
+// @title ToDo App API
+// @version 1.0.0
+// @description API Server for ToDoList Application
+
+// @host localhost:8000
+// @Basepath / 
+
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
+
+
+func main() {
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ivasdtmbb/todo_list_project
+
+go 1.23.4
