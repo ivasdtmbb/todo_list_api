e25d4f7e705837eb84c1d065965a41a5e30900df
admin <ivasdtmbb@gmail.com>
2025-02-19 01:15:30 +1000

    Implement sign-in handler

--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/ivasdtmbb/todo_list_project/internal/todo"
+	
 )
 
 func(h *Handler) signUp(c *gin.Context) {
@@ -26,6 +27,25 @@ func(h *Handler) signUp(c *gin.Context) {
 	})
 }
 
+type signInInput struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
 func(h *Handler) signIn(c *gin.Context) {
-	
+	var input signInInput
+
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+	}
+
+	token, err := h.services.Authorization.GenerateToken(input.Username, input.Password)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return 
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"token": token,
+	})
 }
--- pkg/repository/postgres.go
+++ pkg/repository/postgres.go
@@ -1,6 +1,10 @@
 package repository
 
 // sudo docker run --name=todo_db -e POSTGRES_PASSWORD='qwerty' -p 5436:5432 -d --rm postgres
+// sudo docker exec -it todo_db /bin/bash
+// psgl -U postgres
+//  migrate -path ./schema -database 'postgres://postgres:qwerty@localhost:5436/postgres?sslmode=disable' up
+
 
 import (
 	"fmt"
